6bf5c2a55c47916256d411a116c22e310c4b431d
S-ciz <mushinganyacizungu@gmail.com>
2026-03-06 11:24:08 +0200

    zim location

--- src/services/geolocation.js
+++ src/services/geolocation.js
@@ -1,11 +1,11 @@
 //const MUKFIN_COORDINATES = { x: 28.0, y: -26.08562 }; // x=lng, y=lat
 const ALLOWED_COORDINATES = {
   southAfrica: { x: 28.0, y: -26.08562 },
-  zimbabwe: { x: 31.0926, y: -17.808 },
+  zimbabwe: { x: 31.0926, y: -17.79417 },
   botswana: { x: 25.92492, y: -24.67765 },
   zambia: { x: 28.3295, y: -15.4081 },
 };
-const ALLOWED_DISTANCE = 0.3; // km == 300 meters
+const ALLOWED_DISTANCE = 0.4; // km == 400 meters
 
 function distance(coord, country) {
   const latDiff = (coord.y - ALLOWED_COORDINATES[country].y) * 111.32;
